3266e9c1471ab10e899487d465b65ac90337005e
Your NameNikolay <nikolya_usov@bk.ru>
2021-03-24 01:18:27 +0300

    init: disabled button

--- source/js/validation.js
+++ source/js/validation.js
@@ -3,7 +3,11 @@ const rigistrationContainer = document.querySelector('.registration__container')
 const formTextFieldsets = document.querySelectorAll('.form__fieldset--text');
 const form =document.querySelector('.form');
 const inputPassword = document.querySelector('.form__input-password');
-const passwordRulesList = document.querySelector('.form__pass-rule-list');
+const passwordRulesItems = document.querySelector('.form__pass-rule-list').children;
+const passwordCompareInput = document.querySelector('.form__input-password-compare');
+const buttonSubmit = document.querySelector('.form__button-submit');
+const privacyCheckbox = document.querySelector('.form__checkbox-privacy');
+const formFieldsets = form.querySelectorAll('fieldset');
 
 const RegxpPatterns = {
   EMAIL: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
@@ -12,10 +16,11 @@ const RegxpPatterns = {
 }
 
 const PasswordRules = {
-  SYMBOLS: /^[a-z0-9_]{6,32}$/,
+  SYMBOLS: /^[a-zA-Z0-9_]{6,32}$/,
   NUMBER: /^(?=.*[0-9])/,
-  UPPERLETTER: /^(?=.*[A-Z])/,
+  UPPERLETTER: /^(?=.*[A-Z])(?=.*[a-z])/,
 }
+
 
 const hideButton = (elem) => {
   elem.classList.add('visually-hidden')
@@ -25,6 +30,19 @@ const onRegistrationBtnClick = (evt) => {
   evt.preventDefault();
   hideButton(evt.target)
   rigistrationContainer.classList.remove('visually-hidden')
+}
+
+const unDisabledSubmitButton = () => {
+  const fieldsets = Array.from(formFieldsets);
+
+  const isValid = fieldsets.some((elem) => elem.classList.contains('invalid'));
+  // eslint-disable-next-line no-console
+  console.log(isValid)
+  if (isValid) {
+    buttonSubmit.disabled = true;
+  } else {
+    buttonSubmit.disabled = false;
+  }
 }
 
 const setInvalid = (elem) => {
@@ -77,17 +95,60 @@ const validateTextFieldset = () => {
 }
 
 const onPasswordInput = (evt) => {
-  for (let rule in PasswordRules) {
-    PasswordRules[rule].test(evt.target.value) ?
-      passwordRulesList.classList.add(`form__pass-rule-list--${rule.toLowerCase()}`) :
-      passwordRulesList.classList.remove(`form__pass-rule-list--${rule.toLowerCase()}`);
+  if (evt.target.value.length !== 0 ){
+    for (let rule in PasswordRules) {
+      if (PasswordRules[rule].test(evt.target.value)) {
+
+        for (let item of passwordRulesItems) {
+          if (item.dataset.pattern === rule.toLowerCase()) {
+            item.classList.add('active')
+            item.classList.add('valid')
+          }
+        }
+      }
+
+      else {
+        for (let item of passwordRulesItems) {
+          if (item.dataset.pattern === rule.toLowerCase()) {
+            item.classList.remove('valid')
+          }
+        }
+      }
+
+
+    }
+  }
+  else {
+    for (let item of passwordRulesItems) {
+      item.classList.remove('active');
+    }
+  }
+
+}
+
+const onPasswordCompareChange = (evt) => {
+  if (evt.target.value !== inputPassword.value) {
+    setInvalid(evt.target.closest('.form__fieldset'))
+  } else {
+    setValid(evt.target.closest('.form__fieldset'))
   }
 }
 
 const onFormChange = () => {
+  // eslint-disable-next-line no-console
+  console.log('чендж')
   validateTextFieldset()
+  unDisabledSubmitButton()
 }
 
+const onPrivacyCheckboxChange = (evt) => {
+  evt.target.checked ?
+    setValid(evt.target.closest('.form__fieldset')) :
+    setInvalid(evt.target.closest('.form__fieldset'))
+}
+
+privacyCheckbox.addEventListener('change', onPrivacyCheckboxChange)
 inputPassword.addEventListener('input', onPasswordInput)
 form.addEventListener('change', onFormChange);
 registrationBtn.addEventListener('click', onRegistrationBtnClick);
+passwordCompareInput.addEventListener('change', onPasswordCompareChange)
